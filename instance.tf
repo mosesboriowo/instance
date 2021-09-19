cd7809902fc4ff2573bbbe3fc73391c240235c82
mosesboriowo <85451157+mosesboriowo@users.noreply.github.com>
2021-09-19 08:43:33 +0100

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "AKIA4B32A6MH3NE3LABJ"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "MyFirstInstnace" {
+  ami           = "ami-0bff25b43a4479334"
+  instance_type = "t2.micro"
+}
